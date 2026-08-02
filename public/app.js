3151b4614ede7c7b774704a9d2622d2581a3f729
Arelius-D <daniel.assarelius@kapita.com>
2026-08-03 00:46:34 +0200

    chore: reopen dev at 2.3.0-dev

--- public/app.js
+++ public/app.js
@@ -1942,7 +1942,7 @@ async function checkVersionAndUpdateIndicator() {
   const githubLink = document.querySelector('.footer-github-link');
   if (!githubLink) return;
 
-  let currentVersion = '2.2.0';
+  let currentVersion = '2.3.0-dev';
   try {
     const res = await fetch(apiPath('api/version'));
     if (res.ok) {
