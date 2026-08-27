8cfe162c895d3c1cbabae1647124658a51ef5cd4
Arelius-D <arelius-d@outlook.com>
2026-08-27 12:29:57 +0200

    chore(release): 2.17.0

--- public/app.js
+++ public/app.js
@@ -3603,7 +3603,7 @@ async function checkVersionAndUpdateIndicator() {
   const githubLink = document.querySelector(".footer-github-link");
   if (!githubLink) return;
 
-  let currentVersion = "2.17.0-dev";
+  let currentVersion = "2.17.0";
   try {
     const res = await fetch(apiPath("api/version"));
     if (res.ok) {
